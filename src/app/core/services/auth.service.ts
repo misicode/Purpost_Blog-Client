bbea5ea212b9ec4809a543c1d2d52c9ce49bfbba
misicode <alee_1225@hotmail.com>
2024-02-01 23:19:00 -0500

    feat: update auth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,24 +1,37 @@
 import { HttpClient } from "@angular/common/http";
-import { Injectable, signal } from "@angular/core";
-import { Observable } from "rxjs";
+import { Injectable, computed, signal } from "@angular/core";
+import { Observable, tap } from "rxjs";
 
 import { environment } from "../../../environments/environment";
 
+import { AuthStatus } from "../enums/auth.enum";
 import { User } from "../interfaces/user.interface";
+
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
   private readonly serverUrl: string = environment.serverUrl;
-  private userAuthenticated = signal<User | null>(null);
-  private authStatus = signal<AuthStatus>();
+
+  private _userAuthenticated = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
+  public userAuthenticated = computed(() => this._userAuthenticated);
+  public authStatus = computed(() => this._authStatus);
 
   constructor(private httpClient: HttpClient) {}
 
   login(email: string, password: string): Observable<boolean> {
     return this.httpClient
-      .post<User>(`${ this.serverUrl }/api/auth/login`)
-      .pipe();
+      .post<User>(`${ this.serverUrl }/api/auth/login`, {email, password})
+      .pipe(
+        tap( ({ user, token }) =>
+          this._userAuthenticated.set(user);
+          this._authStatus.set(AuthStatus.authenticated);
+
+          localStorage.setItem("token", token);
+        )
+      );
   }
 }
