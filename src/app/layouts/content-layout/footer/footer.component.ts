58d6753a82c0c097661f775db1207856a46609db
misicode <alee_1225@hotmail.com>
2025-01-21 01:19:40 -0500

    feat: update footer component

--- src/app/layouts/content-layout/footer/footer.component.ts
+++ src/app/layouts/content-layout/footer/footer.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: "app-footer",
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterLink
   ],
   templateUrl: "./footer.component.html",
 })
